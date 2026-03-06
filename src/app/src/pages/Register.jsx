ca1a54016f1ff3438306197bee527d35d66bcc3a
Michael-Kiriya <mikaboss08102008@gmail.com>
2026-03-06 19:39:24 +0300

    Started Register Page
    did few styles, did classes, started workin with new page

--- src/app/src/pages/Register.jsx
+++ src/app/src/pages/Register.jsx
@@ -18,10 +18,10 @@ export default function Register() {
 
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>Register</h1>
-      <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} /><br/>
-      <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} /><br/>
-      <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} /><br/>
+      <h1 class="registerTextRegister">Register</h1>
+      <input class="userRegisterPage" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} /><br/>
+      <input class="emailRegisterPage" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} /><br/>
+      <input class="passwordRegisterPage" placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} /><br/>
       <button onClick={handleRegister}>Register</button>
     </div>
   );
